f6e830f9eb43c687c4b25d9349d6fbe2d5cd1986
Rodolpho Reis <rodolphoreis@live.com>
2024-06-23 10:57:05 +0100

    feat: create method POST

--- src/pages/api/index.tsx
+++ src/pages/api/index.tsx
@@ -3,3 +3,6 @@ function GET(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ message: "GET" });
 }
 
+function POST(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ message: "POST" });
+}
