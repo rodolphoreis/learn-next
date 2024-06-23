ae34dc5ba0fa73a570016bb1ecd75a671a158381
Rodolpho Reis <rodolphoreis@live.com>
2024-06-23 10:55:50 +0100

    feat: create function handleSubmit

--- src/pages/formulario.tsx
+++ src/pages/formulario.tsx
@@ -4,3 +4,8 @@ const Formulario = () => {
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Formulario enviado");
+    console.log(nome, email);
+  };
