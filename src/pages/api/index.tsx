4c954327ed9e532e2159a029fc9171facfc28b72
Rodolpho Reis <rodolphoreis@live.com>
2024-06-23 10:57:42 +0100

    feat: create function handler

--- src/pages/api/index.tsx
+++ src/pages/api/index.tsx
@@ -1,5 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    POST(req, res);
+  } else {
+    GET(req, res);
+  }
+}
 
 function GET(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ message: "GET" });
